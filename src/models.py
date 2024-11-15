7a67f8e6e14b88b186b7825e5840ef588881d0f0
ViniciusLima94 <vinicius.lima.cordeiro@gmail.com>
2024-11-15 17:04:26 +0100

    up

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 ##
 import numpy as np
-from .models_setup import _set_nodes
+from models_setup import _set_nodes, _set_nodes_delayed
 
 
 def _loop(carry, t):
@@ -19,12 +19,40 @@ def _loop(carry, t):
     return phases_history.reshape(N, 1)
 
 
+def _loop_delayed(carry, t):
+
+    N, A, D, omegas, eta, coupling, phases_history = carry
+
+    phases_t = phases_history[:, -1].copy()
+
+    def _return_phase_differences(n, d):
+        phase_differences = phases_history[np.indices(d.shape)[0], d - 1] - phases_t[n]
+        return np.sin(-phase_differences)
+
+    phase_differences = np.stack(
+        [_return_phase_differences(n, d) for n, d in enumerate(D)]
+    )
+
+    # Input to each node
+    Input = (A * phase_differences * coupling[t]).sum(axis=1)
+
+    # Slide History only if the delays are > 0
+    phases_history[:, :-1] = phases_history[:, 1:]
+
+    phases_history[:, -1] = (
+        phases_t + omegas + Input + eta * np.random.normal(0, 1, size=N)
+    )
+
+    return phases_history
+
+
 def KuramotoOscillators(
     A: np.ndarray,
     f: float,
     fs: float,
     eta: float,
     T: float,
+    D: np.ndarray = None,
     coupling: np.ndarray = None,
 ):
 
@@ -33,24 +61,25 @@ def KuramotoOscillators(
     else:
         coupling = np.asarray(coupling)
 
-    # Call setup to initialize nodes and scale A and D matrices
-    N, A, omegas, phases_history, dt = _set_nodes(A, f, fs)
-
-    # Rescale noise with \sqrt{dt}
-    eta = np.sqrt(dt) * eta
+    if isinstance(D, np.ndarray) and D.any():
+        N, A, D, omegas, phases_history, dt = _set_nodes_delayed(A, D, f, fs)
+        carry = [N, A, D, omegas, eta * np.sqrt(dt), coupling, phases_history]
+        _loop_fun = _loop_delayed
+    else:
+        N, A, omegas, phases_history, dt = _set_nodes(A, f, fs)
+        carry = [N, A, omegas, eta * np.sqrt(dt), coupling, phases_history]
+        _loop_fun = _loop
 
     # Stored phases of each node
     phases = np.zeros((N, T))
 
-    carry = (N, A, omegas, eta, coupling, phases_history)
-
     for t in range(T):
         # Update
-        phases_history = _loop(carry, t)
+        phases_history = _loop_fun(carry, t)
         # Store
-        phases[:, t] = phases_history[:, 0]
-        # New carry
-        carry = (N, A, omegas, eta, coupling, phases_history)
+        phases[:, t] = phases_history[:, -1]
+        # New carry only phases history changes
+        carry[-1] = phases_history
 
     phases_fft = np.fft.fft(np.sin(phases), n=T, axis=1)
     TS = np.real(np.fft.ifft(phases_fft))
@@ -74,17 +103,17 @@ if __name__ == "__main__":
 
     # Derived parameters
     N = C.shape[0]
-    D = np.zeros((N, N)) / 1000  # Fixed delay matrix divided by 1000
+    D = 10 * np.ones((N, N))  # Fixed delay matrix divided by 1000
     C = C / np.mean(C[np.ones((N, N)) - np.eye(N) > 0])
     f = 40  # Node natural frequency in Hz
     K = 10  # Global coupling strength
 
     # Generate random placeholder data for TS with shape (3, Npoints)
-    TS, dt_save = KuramotoOscillators(K * C, f, fs, 3.5, T, np.ones(T) * 100)
+    TS, dt_save = KuramotoOscillators(K * C, f, fs, 0 * 3.5, T, D, np.ones(T))
 
     # Extract time series data
     x, y, z = TS[0], TS[1], TS[2]
 
     plt.plot(time, x)
     plt.plot(time, y)
-    plt.plot(time, z)
+    # plt.plot(time, z)
